41e72a2c49e8807dcebcc86372b6d343d2dd41eb
Tasos <tasosbarous@gmail.com>
2018-01-23 20:03:27 +0200

    Routes fix

--- app/app.js
+++ app/app.js
@@ -11,5 +11,5 @@ angular.module('SportsApp', [
 	  		controller:'TeamController',
 	  		templateUrl: 'team/team.html'
 	  	})
-    .otherwise({redirectTo: 'team'});
+    .otherwise({redirectTo: '/'});
 }]);
